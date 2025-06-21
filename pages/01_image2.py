4c43eb2df1bab4ed8e82e40292255dd5d6c6c27c
hellolo2 <comme21@shindong.sen.ms.kr>
2025-06-21 13:12:59 +0900

    Create 01_image2.py

---
+++ pages/01_image2.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExejM4YThubWs0em54Z2x4eXE0cXVmMDliMTk4MzJpaDk1NXhhNGVvYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xdH0MjQ83lGFVv7gjR/giphy.gif")
